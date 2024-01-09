6fbd22ee68765c0fc3db7664dd3af2d2eaf0ace2
freshcodeAlice <freshjs.ofte@gmail.com>
2024-01-09 11:22:27 +0200

    time differense

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const fsPromises = require('fs/promises');
 
-console.log(fsPromises);
 
-/*
-const file = fs.readFileSync('./file.txt', {encoding: 'utf-8'});  // метод, який синхронно читає вміст файлу і при цьому БЛОКУЄ потік виконання
+console.time('1');
 
-*/
+const promise = fsPromises.readFile('./citadelle.txt', 'utf-8');
+promise.then(data => {
+    // console.log(data)
+    console.log(data.length)
+});
 
-// console.log(file);
+console.timeEnd('1');
 
+// const file = fs.readFileSync('./citadelle.txt', {encoding: 'utf-8'});  // метод, який синхронно читає вміст файлу і при цьому БЛОКУЄ потік виконання
+
+// // console.log(file);
+//  console.timeEnd('1');
+
+
+
+ /*
 const promise = fsPromises.readFile('./file.txt', 'utf-8');
 promise.then(data => {
     console.log(data)
 });
 
-
+*/
