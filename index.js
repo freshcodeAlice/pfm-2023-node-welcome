d1df7357f0a3e9bc3b41ee523df8dce9c9d27e67
freshcodeAlice <freshjs.ofte@gmail.com>
2024-01-11 11:07:57 +0200

    user signIn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const http = require('http');
 
+const users = new Map();
+
+function signIn(userObj) {
+    users.set(userObj.email, userObj)
+}
+
+function getListUsers() {
+    return [...users.values()];
+}
+
 
 const server = http.createServer(requestListener); // створюємо сервер, який реагує на запити та передаємо функцію-обробник запитів
 
@@ -17,6 +27,13 @@ function requestListener(request, response) {
             response.statusCode = 200;
             response.statusMessage = 'OK';
             response.end('<html><body>This is a page about our super site</body></html>')
+        } else if (request.url === '/users') {
+            // відповісти на запит списком вже зареєстрованих юзерів
+            response.statusCode = 200;
+            response.statusMessage = 'OK';
+            const usersList = getListUsers();
+            response.end(JSON.stringify(usersList))
+           
         } else {
             response.statusCode = 404;
             response.statusMessage = 'Not Found';
@@ -25,10 +42,34 @@ function requestListener(request, response) {
     }
 
     if (request.method === 'POST') {
-        if (request.url = '/') {
+        if (request.url === '/') {
             response.statusCode = 400;
             response.statusMessage = 'Forbidden';
             response.end('This page has not support this operation');
+        } else if (request.url === '/users') {
+            // Задача - прийняти з запиту дані юзера і відповісти успіхом
+            // Зчитуємо дані і зклеюємо їх докупи
+            let string = '';
+            request.on('data', (chunk) => {
+                string += chunk
+            });
+            request.on('end', () => {
+                // отут всі дані вже завантажено і сталася фінальна подія, яка сигналізує про завершення завантаження інфи
+               
+                // можемо щось робити з тим, що вже лежить в глобальній змінній string
+                const userObj = JSON.parse(string);
+                console.log(userObj);
+                signIn(userObj)
+                // таска: відповісти: John Doe, ви зареєстровані в системі
+                const resString = `${userObj.username} ${userObj.lastName} - you have successfully sign in`;
+                response.statusCode = 201;
+                response.statusMessage = 'Created';
+                response.end(resString);
+            })
+        } else {
+            response.statusCode = 404;
+            response.statusMessage = 'Not Found';
+            response.end('this page not found');
         }
     }
    
